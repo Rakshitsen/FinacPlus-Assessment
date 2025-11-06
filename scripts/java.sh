a2bd37a43fb2f19444f9614a6a6affa75ba1f89f
rakshitsen <rakshitsen1@gmail.com>
2025-11-06 16:05:40 +0530

    script files added

---
+++ scripts/java.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+java -version
+
+
+# java is required for running jenkins and other build tools.
---
+++ scripts/jenkins.sh
@@ -0,0 +1,17 @@
+# jenkins lts verisonion installation
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+
+
+
+# Note
+
+# Weekly: bleeding-edge, faster updates, less stable.
+
+# LTS: slower updates, more reliable, best for production.
